8e32af70182e4d577d3f9f066b58c676e62ecf9d
huoshizhe <huoshizhe@qq.com>
2023-11-27 03:27:43 +0800

    feat:

--- cocos/src/settings.5aae7.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/GameScene.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",resources:"67000",main:"167d1"}};
